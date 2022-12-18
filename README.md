2b61d97fb9270a05671ae9a120f0a329d3974a5c
KseniaLyashchenko <raccoon4682@rambler.ru>
2022-12-18 20:18:19 +0700

    Part1

---
+++ README.md
@@ -0,0 +1,7 @@
+# Part 1 
+
+[Ссылка на загруженные прочтения из NCBI SRA](https://www.ncbi.nlm.nih.gov/sra/?term=SRR8533688)
+[Bash скрипт с реализованным алгоритмом](/input/bash.sh)
+[Результат команды samtools flagstat](/result/final)
+
+
---
+++ input/main.py
@@ -0,0 +1,32 @@
+import os
+from metaflow import FlowSpec, step, card
+
+class LinearFlow(FlowSpec):
+
+    @card
+    @step
+    def start(self):
+        os.system('fastqc SRR8533688.fastq.gz')
+        self.next(self.second)
+
+    @step
+    def second(self):
+        os.system('bwa index ref_genome.fna')
+        self.next(self.third)
+
+    @step
+    def third(self):
+        os.system('bwa mem ref_genome.fna SRR8533688.fastq.gz > result.sam')
+        self.next(self.forth)
+
+    @step
+    def forth(self):
+        os.system('samtools flagstat result.sam > final')
+        self.next(self.end)
+
+    @step
+    def end(self):
+        os.system('python3 quality_check.py final')
+
+if __name__ == '__main__':
+    LinearFlow()
